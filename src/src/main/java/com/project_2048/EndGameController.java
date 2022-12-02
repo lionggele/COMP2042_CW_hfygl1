cf4f39f1bbb8f21968f31b12f2e3ff8c63c1448e
Gele <lionggele40@gmail.com>
2022-12-02 15:54:38 +0800

    End Game Scene and function Update

---
+++ src/src/main/java/com/project_2048/EndGameController.java
@@ -0,0 +1,21 @@
+package com.project_2048;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.text.TextFlow;
+
+public class EndGameController {
+
+    @FXML
+    private Button highScore;
+
+    @FXML
+    private TextFlow score;
+
+    @FXML
+    void nextScene(ActionEvent event) {
+
+    }
+
+}
