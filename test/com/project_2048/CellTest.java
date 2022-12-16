a65e4b62dac9cd922a61ea9110cddc99730d9779
Gele <lionggele40@gmail.com>
2022-12-16 12:51:02 +0800

    Test Cell and GameScene method

--- test/com/project_2048/CellTest.java
+++ test/com/project_2048/CellTest.java
@@ -1,4 +1,17 @@
 package com.project_2048;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class CellTest {
+    @Test
+    void getadded() {
+        {assertEquals(false, Cell.getadded());}
+    }
+
+    @Test
+    void getmoved() {
+        {assertEquals(false, Cell.getmoved());}
+    }
 }
--- test/com/project_2048/GameSceneTest.java
+++ test/com/project_2048/GameSceneTest.java
@@ -2,22 +2,23 @@ package com.project_2048;
 
 import org.junit.jupiter.api.Test;
 
+import static com.project_2048.GameScene.n;
 import static org.junit.jupiter.api.Assertions.*;
 
 class GameSceneTest {
 
     @Test
     void getScore() {
-        {assertEquals(100, GameScene.score);}
+        {assertEquals(0, GameScene.score);}
     }
 
     @Test
     void setN() {
-        {assertEquals(4, GameScene.n);}
+        {assertEquals(4, n);}
     }
 
     @Test
     void getLENGTH() {
-        {assertEquals(162, GameScene.n);}
+        {assertEquals(4, n);}
     }
 }
